15d7fb5a4024a7e1c666a62d8d1fbc09267f76b3
danish <dakhannish@gmail.com>
2025-04-02 06:32:24 +0800

    Quick fix pro

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,4 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/',
-  build: {
-    outDir: 'dist',
-    sourcemap: true,
-    minify: 'terser'
-  }
 })
